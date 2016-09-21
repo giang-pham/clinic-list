eca31c38790d2f7888a131cd2c7105ac0f051bc2
Giang <hoang_pham@aviva-asia.com>
2016-09-21 17:13:14 +0800

    convert clinic raw pdf to list clinic object

--- convert.js
+++ convert.js
@@ -1,9 +1,11 @@
 var http = require('http');
 var https = require('https');
 var fs = require('fs');
+var sanitizer = require('sanitizer');
 
 var url = "https://www.aviva.com.sg/pdf/Aviva_MyBenefits_Clinic_Listing.pdf";
 var dest = "pdf/clinic.pdf"
+var raw_json = 'clinic_raw.json';
 
 var download = function(url, dest, cb) {
   var file = fs.createWriteStream(dest);
@@ -24,10 +26,77 @@ var convertPdf = function(file) {
 
   pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
   pdfParser.on("pdfParser_dataReady", pdfData => {
-     fs.writeFile("clinic.json", JSON.stringify(pdfData));
+     fs.writeFile(raw_json, convertJson(pdfData));
   });
-  
+
   pdfParser.loadPDF(file);
+}
+
+// convert raw pdf json to understandable clinic json in order to consume
+var convertJson = function(raw_json) {
+  var json = raw_json;
+  var result = '';
+  var pages = json.formImage.Pages;
+  var index = 0;
+  var clinics = [];
+  var clinic = {};
+  var columns = {};
+
+  for (var j = 0; j < 1; j++) {
+    var page = pages[j];
+    var texts = page.Texts;
+
+    for(var i = 0; i < texts.length; i++) {
+      var cell = texts[i];
+      var cellText = decodeURIComponent(cell.R[0].T);
+
+      console.log(cellText);
+
+      var cellX = cell.x;
+
+      if (cellText == 'S/N'||
+        cellText == 'AVIVA CODE' || cellText == 'REMARKS' || cellText == 'TELEPHONE'
+        || cellText == 'FAX NO.' || cellText == 'ADDRESS 1' || cellText == 'ADDRESS 2'
+        || cellText == 'POSTAL' || cellText == 'PUBLIC HOLIDAY' || cellText == 'SATURDAY'
+        || cellText == 'SUNDAY' || cellText == 'ZONE' || cellText == 'ESTATE') {
+          columns[cellX] = cellText;
+        }
+    }
+    console.log(columns);
+  };
+
+  for (var j = 0; j < pages.length; j++) {
+    var page = pages[j];
+    var texts = page.Texts;
+
+    for(var i = 0; i < texts.length; i++) {
+      var cell = texts[i];
+      var cellText = decodeURIComponent(cell.R[0].T);
+
+      var cellX = cell.x;
+
+      var text = decodeURIComponent(cellText);
+      if(isNumeric(text)) {
+        if(text - 1 == index) {
+          clinics.push(clinic);
+          index = text;
+          clinic = {};
+          for (var x in columns) {
+            clinic[columns[x]] = '';
+          }
+        }
+      }
+      if(columns[cellX] != null) {
+        clinic[columns[cellX]] += text;
+      }
+    }
+  }
+  return JSON.stringify(clinics);
+
+};
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 download(url, dest, convertPdf);
--- index.js
+++ index.js
@@ -7,28 +7,27 @@ function isNumeric(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-app.get('/', function (req, res) {
-  var json = JSON.parse(fs.readFileSync('clinic.json', 'utf8'));
-  var result = '';
-  var pages = json.formImage.Pages;
-  var index = 0;
-  for (var j = 0; j < pages.length; j++) {
-    var page = pages[j];
-    var texts = page.Texts;
-    for(var i = 0; i < texts.length; i++) {
-      var r = texts[i].R[0];
-      var text = decodeURI(r.T);
-      if(text.length < 4 && isNumeric(text)) {
-        if(text - 1 == index) {
-          index = text;
-          result += '<hr>';
-        }
-      }
-      result += text + '<br/>';
-    }
-  }
-
-  res.send(result);
+app.get('/clinic_list', function (req, res) {
+  var json = JSON.parse(fs.readFileSync('clinic_raw.json', 'utf8'));
+  // var result = '';
+  // var pages = json.formImage.Pages;
+  // var index = 0;
+  // for (var j = 0; j < pages.length; j++) {
+  //   var page = pages[j];
+  //   var texts = page.Texts;
+  //   for(var i = 0; i < texts.length; i++) {
+  //     var r = texts[i].R[0];
+  //     var text = decodeURI(r.T);
+  //     if(text.length < 4 && isNumeric(text)) {
+  //       if(text - 1 == index) {
+  //         index = text;
+  //         result += '<hr>';
+  //       }
+  //     }
+  //     result += text + '<br/>';
+  //   }
+  // }
+  res.send(json);
 });
 
 app.listen(3000, function () {
